691426200c39b2d60cccd49f2111ae7af8545143
Bernardo Behring <bernardo.behring@softplan.com.br>
2019-07-31 13:53:12 -0300

    Model comments

--- StarWarsDestiny/Card.cs
+++ StarWarsDestiny/Card.cs
@@ -10,13 +10,15 @@ namespace StarWarsDestiny.Model
         public Artist Artist { get; set; }
         public Affiliation Affiliation { get; set; }
         public Color Color { get; set; }
-        public Type Type { get; set; }
+        public ICollection<Type> Type { get; set; }
         public Rarity Rarity { get; set; }
         public Die Die { get; set; }
         public Set Set { get; set; }
-        public ICollection<Legality> Legalities { get; set; }
-        public ICollection<BalanceForce> BalanceForces { get; set; }
+        
         public ICollection<Keyword> Keywords { get; set; }
         public byte[] Image { get; set; }
+        //TODO COLOCAR EM UM OUTRO LUGAR
+        public ICollection<Legality> Legalities { get; set; }
+        public ICollection<BalanceForce> BalanceForces { get; set; }
     }
 }
